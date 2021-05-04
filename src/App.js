c9f58e43d08cd2744e49f9901a4c34d55be485f6
Kevin Meldau <me@kevinmeldau.com>
2021-05-04 10:14:42 -0400

    Setup base styles and connect to chat-engine

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="d3a516ec-1dc5-4d13-8d75-0c8beb98b445"
+      userName="Kevin_Meldau"
+      userSecret="123123"
+    />
+  );
+};
+
+export default App;
