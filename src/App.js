3c2452691f4a5188216e518dc05318946794c19e
Kevin Meldau <me@kevinmeldau.com>
2021-05-04 11:38:24 -0400

    Created components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+
 import "./App.css";
 
 const App = () => {
@@ -8,6 +10,7 @@ const App = () => {
       projectID="d3a516ec-1dc5-4d13-8d75-0c8beb98b445"
       userName="Kevin_Meldau"
       userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
